8046c550428ca5b248fddacc4eb2cb1f7d258a17
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-26 23:39:03 +0600

    updated file

--- src/components/HeroModel/HeroLight.jsx
+++ src/components/HeroModel/HeroLight.jsx
@@ -3,9 +3,9 @@ const HeroLight = () => {
 
     return (
         <>
-            {/* Lighting */}
-            <ambientLight intensity={0.3} color="#1a1a40" />
-            <directionalLight position={[5, 5, 5]} intensity={2} />
+            <spotLight>
+
+            </spotLight>
         </>
     );
 };
